b1ac363d62d3af053e9131c5905bb6c835e8950f
Hunter Krieger <hunterk31@hotmail.com>
2018-10-10 17:24:00 -0400

    change

--- podcast/settings.py
+++ podcast/settings.py
@@ -79,11 +79,6 @@ WSGI_APPLICATION = 'podcast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql'
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
